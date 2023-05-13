c526f6cb8249123a72bb197430af142e9bb5858e
Paweł <pawelo247@icloud.com>
2023-05-13 18:31:31 +0200

    Add style and script for nav-menu-mobile.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+let logoWriting = document.getElementById("logo-writing");
+let circle = document.getElementById("circle");
+let circleMark = document.getElementById("circle-mark");
+
 let hamburgerBtn = document.getElementById("hamburger-icon");
+let navList = document.getElementById("nav-menu-list");
+let socialMedia = document.getElementById("social-media-nav");
+
+let header = document.querySelector("header");
+let nav = document.querySelector("nav");
+let main = document.querySelector("main");
+
+let headerHeight = 0;
 
 let simpleBookmarkingP = document.getElementById("simple-bookmarking");
 let speedySearchingP = document.getElementById("speedy-searching");
@@ -12,16 +24,46 @@ let question4 = document.getElementById("question-4");
 let input = document.getElementById("mail-input");
 let contactUsBtn = document.getElementById("conntact-btn");
 
+let iconError = document.getElementById("icon-error");
 let bg = document.getElementById("not-valid-bg");
 let notValidP = document.getElementById("not-valid-mail");
 
-const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 let showNavList = () => {
     if(hamburgerBtn.src.endsWith("images/icon-hamburger.svg")) {
+        headerHeight = header.offsetHeight;
+        main.style.top = headerHeight + "px";
+
         hamburgerBtn.src = "images/icon-close.svg";
+
+        document.body.style.overflow = "hidden";
+
+        logoWriting.classList.add("set-logo-color");
+        circle.classList.add("set-logo-color");
+        circleMark.classList.add("set-circle-mark");
+
+        header.classList.add("show-nav-header");
+        nav.classList.add("show-nav-nav");
+        main.classList.add("show-nav-main");
+
+        navList.classList.remove("hide");
+        socialMedia.classList.remove("hide");
     } else {
         hamburgerBtn.src = "images/icon-hamburger.svg";
+
+        document.body.style.overflow = "auto";
+
+        logoWriting.classList.remove("set-logo-color");
+        circle.classList.remove("set-logo-color");
+        circleMark.classList.remove("set-circle-mark");
+
+        header.classList.remove("show-nav-header");
+        nav.classList.remove("show-nav-nav");
+        main.classList.remove("show-nav-main");
+
+        navList.classList.add("hide");
+        socialMedia.classList.add("hide");
     }
 }
 
@@ -59,9 +101,11 @@ let showAnswer = (questionNumber) => {
 let checkEmail = () => {
     let mail = input.value;
     if(regex.test(mail)) {
+        iconError.classList.add("hide");
         bg.classList.add("hide");
         notValidP.classList.add("hide")
     } else {
+        iconError.classList.remove("hide");
         bg.classList.remove("hide");
         notValidP.classList.remove("hide")
     }
@@ -88,3 +132,23 @@ question4.addEventListener("click", () => {showAnswer(4)});
 
 
 contactUsBtn.addEventListener("click", () => {checkEmail()});
+
+window.addEventListener("resize", () => {
+    if(hamburgerBtn.src.endsWith("images/icon-close.svg") && window.innerWidth > 768) {
+        hamburgerBtn.src = "images/icon-hamburger.svg";
+
+        document.body.style.overflow = "auto";
+
+        logoWriting.classList.remove("set-logo-color");
+        circle.classList.remove("set-logo-color");
+        circleMark.classList.remove("set-circle-mark");
+
+        header.classList.remove("show-nav-header");
+        nav.classList.remove("show-nav-nav");
+        main.classList.remove("show-nav-main");
+
+        navList.classList.add("hide");
+        socialMedia.classList.add("hide");
+
+    }
+});
